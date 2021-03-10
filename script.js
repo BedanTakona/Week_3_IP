c3dca5b8e80c75d3c53765ad9a4159f5dcc5b2ca
BedanTakona <bedanmaison35@gmail.com>
2021-03-11 02:36:39 +0300

    Added a toggle jquery method

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    $("p").click(function(){
-      $(this).hide();
-    });
+
+  $("#design").click(function(){
+    $("#show_design").toggle();
+    $(".hide_des").toggle();
+  })
+
+  $("#development").click(function(){
+    $("#show_dev").toggle();
+    $(".hide_dev").toggle();
+  })
+
+  $("#product").click(function(){
+    $("show_product").toggle();
+    $(".hide_product").toggle();
+  })
+
+
   });
